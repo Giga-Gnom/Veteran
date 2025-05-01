139491cb3ad529c6022ee866870ca5982407ec91
Ilya <ilyadv06@gmail.com>
2025-05-01 19:56:39 +0300

    sao novao areas name add to map

--- src/components/Windows/MapWindow/DistrictWindows/SAOWindow/SAOWindow.jsx
+++ src/components/Windows/MapWindow/DistrictWindows/SAOWindow/SAOWindow.jsx
@@ -21,13 +21,27 @@ const SAOWindow = () => {
                 <div className={styles.mapContainer_map}>
                     <svg viewBox="425 75 150 600" className={styles.detailedMap}>
                         {district.area?.map((area) => ( 
-                            <path
-                                key={area.id}
-                                d={area.path}
-                                className={styles.area}
-                            />
+                            <g key={area.id}>
+                                <path
+                                    d={area.path}
+                                    className={styles.area}
+                                />
+                                <text x={area.center.x} y={area.center.y} className={styles.area_label}>{area.id}</text>
+                            </g>
                         ))}
                     </svg>
+                </div>
+                <div className={styles.mapContainer_legend}>
+                    <div className={styles.mapContainer_legend_title}>
+                        <h2 className={styles.mapContainer_legend_title_h2}>
+                            {district.title}
+                        </h2>                    
+                    </div>
+                    {district.area.map((area) => (
+                        <div className={styles.mapContainer_legend_block} key={area.id}>
+                            <p>{area.id}. {area.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className={styles.container_button}>
