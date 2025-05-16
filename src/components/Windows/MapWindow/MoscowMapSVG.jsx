d13df8bc3bc788ffda2d8785790611b8c9689cdc
Ilya <ilyadv06@gmail.com>
2025-05-16 11:36:58 +0300

    title head districts

--- src/components/Windows/MapWindow/MoscowMapSVG.jsx
+++ src/components/Windows/MapWindow/MoscowMapSVG.jsx
@@ -11,15 +11,17 @@ const MoscowMapSVG = ({ onDistrictClick, activeDistrict }) => {
       };
     return(
         <svg viewBox="0 0 1000 800" className={styles.map}>
-            {districtsData.map((district,index) => (
-        <path
-          key={index}
-          d={district.path}
-          data-id={district.id}
-          className={styles.district}
-          onClick={handleClick}
-        />
-      ))}
+          {districtsData.map((district,index) => (
+            <g key={index}>
+              <path
+                d={district.path}
+                data-id={district.id}
+                className={styles.district}
+                onClick={handleClick}
+              />
+              <text x={district.center.x} y={district.center.y} className={styles.map_text}>{district.name}</text>
+            </g>
+          ))}
         </svg>
     )
 }
