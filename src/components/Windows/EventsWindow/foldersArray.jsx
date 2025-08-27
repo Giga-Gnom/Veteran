c83742ce09bcf1f0451838e8ec14181109c97cd6
Ilya <ilyadv06@gmail.com>
2025-08-27 12:54:47 +0300

    photo add

--- src/components/Windows/EventsWindow/foldersArray.jsx
+++ src/components/Windows/EventsWindow/foldersArray.jsx
@@ -26,4 +26,74 @@ export const foldersArray = [
         name: "2014-2015",
         path: ""
     },
+    {
+        img: folderImg,
+        name: "2016-2017",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2018",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2019-2020",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2021-2022",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2023",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2024",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "2025",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Выездное совещание в Совете ветеранов ЗАО",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Заседание медицинской комиссии МГСВ",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Инструктивно-методическое занятие",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Конкурс на лучшую районную медкомиссию",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Конкурс-выставка цветов",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Круглый стол Никто, кроме нас",
+        path: ""
+    },
+    {
+        img: folderImg,
+        name: "Участие в показе фильма об участниках СВО",
+        path: ""
+    },
 ]
